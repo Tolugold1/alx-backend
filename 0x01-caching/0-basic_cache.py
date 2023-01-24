8dc05f90150d746114dfba3c02f6ba32ca8d3913
Tolulope <toluadelekesteve@gmail.com>
2023-01-24 12:14:54 +0000

    caching task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+caching system
+"""
+BaseCaching = __import__("BaseCaching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """caching system"""
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data
+        the item value for the key key"""
+        if (key is not None and item is not None):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in
+        self.cache_data linked to key"""
+        if (key is None or key not in self.cache_data):
+            return None
+        return self.cache_data[key]

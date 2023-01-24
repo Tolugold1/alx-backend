e89b6a46c1d956628a4c44db1fb2606e55de54da
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-01-24 04:16:24 -0800

    Add files via upload

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+class FIFOCache that inherits from
+BaseCaching and is a caching system
+"""
+BaseCaching = __import__("BaseCaching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """class fifo caching"""
+    
+    def __init__(self):
+        """Initializing class FIFOCaching"""
+        super().__init__()
+        
+    
+    def put(self, key, item):
+        """assign to the dictionary
+        self.cache_data the item value
+        for the key key"""
+        if (key is not None and item is not None):
+            self.cache_data[key] = item
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS - 1):
+            lst = []
+            for x in self.cache_data.keys():
+                lst.append(x)
+            del self.cache_data[lst[0]]
+            print(f"DISCARD: {lst[0]}")
+
+    def get(self, key):
+        """return the value in
+        self.cache_data linked to key"""
+        if (key is None or key not in self.cache_data):
+            return None
+        return self.cache_data[key]

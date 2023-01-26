a4661c10375cc8cde6ed4f0f742b7c14f07f410b
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-01-26 00:57:20 -0800

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -16,20 +16,19 @@ class MRUCache(BaseCaching):
         """assign to the dictionary
         self.cache_data the item value
         for the key key"""
-        if Key and item:
-            if (len(self.cache_data) < BaseCaching.MAX_ITEMS):
-                if (key in self.cache_data):
-                    del self.cache_data[key]
+        if (len(self.cache_data) < BaseCaching.MAX_ITEMS):
+            if (key in self.cache_data):
+                del self.cache_data[key]
+            self.cache_data[key] = item
+        else:
+            if key in self.cache_data:
+                del self.cache_data[key]
                 self.cache_data[key] = item
             else:
-                if key in self.cache_data:
-                    del self.cache_data[key]
-                    self.cache_data[key] = item
-                else:
-                    last_key = list(self.cache_data.keys())[-1]
-                    del self.cache_data[last_key]
-                    self.cache_data[key] = item
-                    print(f"DISCARD: {last_key}")
+                last_key = list(self.cache_data.keys())[-1]
+                del self.cache_data[last_key]
+                self.cache_data[key] = item
+                print(f"DISCARD: {last_key}")
 
     def get(self, key):
         """return the value in

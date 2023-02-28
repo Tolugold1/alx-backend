441249f0e7e6b163a4d0b76bdbe2f53a8a59edcf
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-02-28 05:36:29 -0800

    Update 0-redis_client.js

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,14 +1,8 @@
-import { createClient } from "redis";
+import redis from 'redis';
+const client = redis.createClient();
 
-const client = createClient();
-
-client.on('error', (error) => {
-  if (error) {
-    console.log(`Redis client not connected to the server: ${error}`);
-  }
-})
-
-const conn = client.connect();
-conn.then(resp => {
-  console.log('Redis client connected to the server');
+client.on("error", (error) => {
+  if (error) console.log(`Redis client not connected to the server: ${error}`)
+}).on('ready', () => {
+    console.log('Redis client connected to the server');
 });
